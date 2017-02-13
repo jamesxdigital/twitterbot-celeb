095ccbb000480dd86b0d53c17e32bdde94d61e12
jmltn <james.milton@me.com>
2017-02-13 19:16:14 +0000

    first commit

---
+++ bot.js
@@ -0,0 +1,65 @@
+console.log('The bot is starting');
+
+var Twit = require('twit');
+
+var config = require('./config');
+
+console.log(config);
+var T = new Twit(config);
+
+var fs = require('fs');
+var questions = fs.readFileSync('./questions.txt').toString().split("\r");
+var accounts = fs.readFileSync('./accounts.txt').toString().split("\n");
+var thoughts = fs.readFileSync('./thoughts.txt').toString().split("\r");
+
+
+
+
+function doSomething() {
+	randomNumberTweet = Math.floor((Math.random() * 10) + 0);
+
+	randomNumberQuestions = Math.floor((Math.random() * questions.length) + 0);
+	randomNumberAccounts = Math.floor((Math.random() * accounts.length) + 0);
+	randomNumberThoughts = Math.floor((Math.random() * thoughts.length) + 0);
+
+	randomQuestion = questions[randomNumberQuestions].toLowerCase();
+	randomAccount = accounts[randomNumberAccounts];
+	randomThought = thoughts[randomNumberThoughts].toLowerCase();
+
+
+	randomQuestionTweet = '@' + randomAccount + " " + randomQuestion;
+	randomThoughtTweet = randomThought;
+
+	if (randomNumberTweet < 6){
+		randomTweet = randomThoughtTweet;
+	}else{
+		randomTweet = randomQuestionTweet;
+	}
+
+
+	T.post('statuses/update', { status: randomTweet}, function(err, data, response) {
+	  console.log("------------------------------------------------------------------------");
+	  console.log(randomTweet);
+	  console.log(data);
+	});
+}
+
+
+(function loop() {
+    var rand = randomNumberTweet = Math.floor((Math.random() * 1800000) + 300000);
+    setTimeout(function() {
+            doSomething();
+            loop();  
+    }, rand);
+}());
+
+
+
+
+
+
+
+
+
+
+
---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  consumer_key:         'nfwICPDQe3jr0fKeHuKr6RlsG',
+  consumer_secret:      '0n7YgrNRnaJgLYQKDMfjIGFWollme5vplLKWIPm7UOrsas8h7F',
+  access_token:         '823644165011570688-jPJmvkiCA37YZ2pYBqdfAysgxAGaplq',
+  access_token_secret:  'b3A9gJp8CDtSS35Q6PvtChgYWDLGHmZYf3qH0TNlntShH',
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests. 
+}
